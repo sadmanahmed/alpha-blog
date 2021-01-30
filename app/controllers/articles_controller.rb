aa34f5e5009f4cd151579269a7e02caee36f6308
sadmanahmed <abirsadmanahmed177@gmail.com>
2021-01-31 04:14:16 +0600

    Added all controller and initiated DRY method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article , only: [:show,:edit,:destroy,:update]
+
   def show
-    @article = Article.find(params[:id])
+
 
   end
 
@@ -10,13 +13,49 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article= Article.new
+  end
+
+  def edit
 
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title,:description))
-    @article.save
-    redirect_to @article
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
 
+
+
+  def update
+
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
+  def destroy
+
+    @article.destroy
+    redirect_to articles_path
   end
+
+
+private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title,:description)
+  end
+
+
+end
