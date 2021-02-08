4ce751dab7443969c3f46e938fea43164fb57172
sadmanahmed <abirsadmanahmed177@gmail.com>
2021-02-09 05:41:14 +0600

    Categories association done and working properly

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_06_200428) do
+ActiveRecord::Schema.define(version: 2021_02_08_205305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
